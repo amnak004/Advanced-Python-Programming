875f8ef4ea2a60a16e0d048d2a45dded1c60adad
amnak004 <amnaahmed0029@gmail.com>
2026-08-04 19:04:51 +0500

    Add Exercise 5

---
+++ Week 1/Day 1/Exercise_5.py
@@ -0,0 +1,10 @@
+# Expected Exception:
+# ZeroDivisionError
+
+print("Step 1: Program started")
+print("Step 2: Everything is working")
+print("Step 3: About to cause an error")
+
+print(10 / 0)  # This line raises ZeroDivisionError
+
+print("Step 5: This line will never execute")
